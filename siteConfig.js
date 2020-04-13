f6210efeea771cd00116d7a5bea0100b6fb0e4b4
Jurrian <jurrianlammerts@hotmail.com>
2020-04-13 17:10:14 +0200

    gatbsy beginner

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "Urban Timber",
+  shortName: "Urban Timber",
+  description: "Een passie voor vakmanschap",
+  url: "https://urbantimber.nl", // Domain of your site without prefix!
+  prefix: "/",
+  author: "",
+  twitter: ""
+};
